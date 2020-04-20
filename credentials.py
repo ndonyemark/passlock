eabb1e59254cdeb4f0da95940ce1b15111c0979e
ndonye mark <ndonyemark@gmail.com>
2020-04-21 00:54:54 +0300

    add Credentials test class and test init method

--- credentials.py
+++ credentials.py
@@ -1,1 +1,8 @@
 class Credentials:
+
+    credentials_list = []
+
+    def __init__(self, username, account_name, account_password):
+        self.username = username
+        self.account_name = account_name
+        self.account_password = account_password
--- credentials_test.py
+++ credentials_test.py
@@ -1,2 +1,12 @@
 import unittest
 from credential import Credentials
+
+class Credentials_test(unittest.TestCase):
+    
+    def setUp(self):
+
+        """
+        instructions to run before any test is done
+        """
+
+        self.new_credentials = Credentials("ndonyemark", "instagram", "bossbaby")
