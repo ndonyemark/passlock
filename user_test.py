f0565a7016f00517b380199b7e8c517bd3e08e0a
ndonye mark <ndonyemark@gmail.com>
2020-04-21 09:46:14 +0300

    add the user test class and setUP

--- user_test.py
+++ user_test.py
@@ -1,2 +1,12 @@
 import unittest
 from users import Users
+
+class Users_test(unittest.TestCase):
+    
+    def setUp(self):
+        
+        """
+        instructions to run before any test is done
+        """
+        
+        self.new_user = Users("Toni", "Bossbaby")
