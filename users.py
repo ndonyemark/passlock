9df146a42a683f56edb8770dda5cc0e733665508
ndonye mark <ndonyemark@gmail.com>
2020-04-21 09:51:48 +0300

    add users list and init method

--- users.py
+++ users.py
@@ -1,3 +1,11 @@
 from credential import Credentials
 
 class Users:
+    users_list = []
+
+    def __init__(self, username, password):
+        """
+        method to 
+        """
+        self.username = username
+        self.password = password
