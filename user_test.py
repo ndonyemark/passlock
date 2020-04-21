6ec93ee5fec46fe65d1669c6cc801105c26baf82
ndonye mark <ndonyemark@gmail.com>
2020-04-21 09:49:13 +0300

    add the test find users method

--- user_test.py
+++ user_test.py
@@ -48,3 +48,15 @@ class Users_test(unittest.TestCase):
         test_user.save_user()
         self.new_user.del_user()
         self.assertEqual(len(Users.users_list), 1)
+    
+    def test_find_user_by_username(self):
+        
+        """
+        method to test if you can lookup a user based on the username
+        """
+
+        self.new_user.save_user()
+        test_user = Users("Mark", "justmark")
+        test_user.save_user()
+        found_user =Users.find_user("Mark")
+        self.assertEqual(found_user.username, test_user.username)
